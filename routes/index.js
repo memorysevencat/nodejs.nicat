b710f672df2fa36f1f384db3be43016616371a20
memorysevencat <490542807@qq.com>
2016-01-28 21:33:35 +0800

    https://github.com/memorysevencat/nodejs.nicat.git

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//nicat set the homepage
+router.get('/', function (req, res, next) {
+    res.render('home', {title: 'homepage'});
+});
+//nicat login
+router.get('/login', function (req, res, next) {
+    res.render('login', {title: 'login'});
+});
+var user = {
+    username: 'nicat',
+    password: 'nicat'
+};
+router.post('/login', function (req, res, next) {
+    if (req.body.username === user.username && req.body.password === user.password) {
+        req.session.user = user;
+        res.render('home', {title: 'welcome to homepage'});
+    } else {
+        res.render('login', {title: 'failed to login.try again'});
+    }
+});
+
+router.get('/index', function (req, res, next) {
+    res.render('index', {title: 'index'});
 });
 
 module.exports = router;
