fa295d2767ceac5559266f3582ce20ae99ba8020
memorysevencat <490542807@qq.com>
2016-01-29 21:02:03 +0800

    in up out

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,17 @@ var router = express.Router();
 /* GET home page. */
 //nicat set the homepage
 router.get('/', function (req, res, next) {
-    res.render('home', {title: 'homepage'});
+    res.render('signin', {title: 'welcome! sign in'});
 });
 //nicat
 router.get('/signin', function (req, res, next) {
     res.render('signin', {title: 'signin'});
 });
 router.get('/signup', function (req, res, next) {
-    res.render('signup', {title: 'signup'});
+    res.render('signup', {title: 'welcome! sign up'});
+});
+router.get('/signout', function (req, res, next) {
+    res.render('signin', {title: ''});
 });
 var user = {
     username: 'nicat',
@@ -20,12 +23,17 @@ var user = {
 router.post('/signin', function (req, res, next) {
     if (req.body.username === user.username && req.body.password === user.password) {
         req.session.user = user;
-        res.render('home', {title: 'welcome to homepage'});
+        res.render('home', {title: 'success!welcome :' + req.body.username, content: 'u can check the session'});
     } else {
         res.render('signin', {title: 'failed to login.try again'});
     }
 });
-
+router.post('/signup', function (req, res, next) {
+    user.username = req.body.username;
+    user.username = req.body.password;
+    req.session.user = user;
+    res.render('home', {title: 'success!welcome :' + req.body.username, content: 'u can check the session'});
+});
 router.get('/index', function (req, res, next) {
     res.render('index', {title: 'index'});
 });
