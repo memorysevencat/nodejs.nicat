d09266814c4b4513b13a08381ab3270cdfc15d27
seven.cat <490542807@qq.com>
2016-04-07 21:11:01 +0800

    Update Events.js
    Events API End

--- api_practise/Events.js
+++ api_practise/Events.js
@@ -50,11 +50,88 @@
 //myEmitter.emit('event');
 //myEmitter.emit('event');
 
-const MyEmitter=require('events');
-const myEmitter=new MyEmitter();
-myEmitter.on('event',(a,b)=>{
-   setImmediate(()=>{
-      console.log('this happens asynchronously');
-   });
-});
-myEmitter.emit('event','a','b');
+//const MyEmitter=require('events');
+//const myEmitter=new MyEmitter();
+//myEmitter.on('event',(a,b)=>{
+//   setImmediate(()=>{
+//      console.log('this happens asynchronously');
+//   });
+//});
+//myEmitter.emit('event','a','b');
+
+//const MyEmitter = require('events');
+//myEmitter=new MyEmitter();
+//myEmitter.emit('error',new Error('whoops'));
+
+//const MyEmitter=require('events');
+//const myEmitter=new MyEmitter();
+//process.on('uncaughtException',(err)=>{
+//    console.log('whoops! there was error!');
+//});
+//
+//myEmitter.emit('error',new Erroe('whoops!'));
+
+//const MyEmitter=require('events');
+//const myEmitter=new MyEmitter();
+//
+//myEmitter.once('newListener',(event,listener)=>{
+//    if(event==='event')
+//{
+//    myEmitter.on('event',()=>{
+//        console.log('b');
+//    });
+//}
+//});
+//myEmitter.on('event',()=>{
+//   console.log('a');
+//});
+//myEmitter.emit('event');
+//const MyEmitter=require('events');
+//const emitter=new MyEmitter();
+//emitter.setMaxListeners(emitter.getMaxListeners()+1);
+//emitter.once('event',()=>{
+//    console.log('memorysevencat');
+//    emitter.setMaxListeners(Math.max(emitter.getMaxListeners()-1,0))
+//});
+//emitter.on('event')
+
+//server.on('connection',(stream)=>{
+//    console.log('something connected');
+//});
+//
+//console.log(util.inspect(server.listeners('connection')));
+
+
+//const MyEmitter = require('events');
+//const myEmitter = new MyEmitter();
+//myEmitter.once('newListener',(event,listener)=>{
+//    if(event==='event'){
+//    myEmitter.on('event',()=>{
+//        console.log('B');
+//    });
+//};
+//});
+//
+//myEmitter.on('event',()=>{
+//    console.log('A');
+//});
+//
+//myEmitter.emit('event');
+
+
+const MyEmitter = require('events');
+const myEmitter = new MyEmitter();
+var callbackA = () =>
+{
+    console.log('A');
+    myEmitter.removeListener('event', callbackB);
+};
+var callbackB=()=>{
+    console.log('B');
+};
+myEmitter.on('event',callbackA);
+myEmitter.on('event',callbackB);
+
+myEmitter.emit('event');
+console.log('#');
+myEmitter.emit('event');
