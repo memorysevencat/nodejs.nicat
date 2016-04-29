e5dbb6cd8b98d2d8b3036d49484ad4b91f66e26f
seven.cat <490542807@qq.com>
2016-04-29 23:40:40 +0800

    Create net.js

---
+++ api_practise/net.js
@@ -0,0 +1,13 @@
+const net = require('net');
+var server = net.createServer((socket) => {
+        socket.end('goodbye');
+}).
+on('error', (err) => {
+    throw err;
+})
+;
+
+server.listen(() => {
+    address = server.address();
+console.log('opened server on %j', address);
+});
