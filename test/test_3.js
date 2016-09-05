8119e8a324422c20e6230bdeaec386521f2c2fa6
seven.cat <490542807@qq.com>
2016-09-05 19:45:04 +0800

    Create test_3.js

---
+++ test/test_3.js
@@ -0,0 +1,44 @@
+/**
+ * Created by lintao on 16-9-5.
+ */
+const util = require('util');
+const events = require('events');
+const Anythin = function (name) {
+    this.name = name;
+}
+
+util.inherits(Anythin, events.EventEmitter);
+
+var cat = new Anythin('black cat');
+cat.on('activity', function (activity) {
+    console.log(this.name + ' ' + activity);
+});
+
+var mouse = new Anythin('black mouse');
+mouse.on('activity', function (activity) {
+    console.log(this.name + ' ' + activity);
+});
+
+var owner = new Anythin('owner');
+owner.on('activity', function (activity) {
+    console.log(this.name + ' ' + activity);
+});
+
+var child = new Anythin('child');
+child.on('activity', function (activity) {
+    console.log(this.name + ' ' + activity);
+});
+
+setTimeout(function () {
+    cat.emit('activity', 'sleepy');
+    mouse.emit('activity', 'move');
+    owner.emit('activity', 'appear');
+    child.emit('activity', 'cry');
+}, 3000);
+//
+>nodev6 test5.js
+//
+ black cat sleepy
+ black mouse move
+ owner appear
+ child cry
